449a94d00f4eeb3489272d3bd15dd66711f6ec33
Andres715 <sumale920@gmail.com>
2022-05-02 21:40:39 -0600

    ventana modal

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-home',
@@ -9,13 +10,48 @@ export class HomeComponent implements OnInit {
 
   public user:string = "";
 
+  public messages:any = [];
+  public messagesInOrden:any = [];
+
+  public messageJSON:any = {};
+  public message:string = "";
+
+  public contador:number = 0;
+  
+
   constructor() { }
 
   ngOnInit(): void {
-    let miUser = prompt("ingresa tu nombre");
-    if(miUser){
-      this.user = miUser;
+    //this.ingresarNombre();
+  }
+
+  enter(){
+    let backdrop = document.getElementById("backdrop");
+    let modal = document.getElementById("modal");
+
+    if(!this.user){
+      alert("Ingresa un user valido");
+    } else if(backdrop && modal){
+      backdrop.style.display = "none";
+      modal.style.display = "none";
     }
+  }
+
+  sendMessage(){
+    this.messageJSON.user = this.user + " " + this.contador;
+    this.messageJSON.message = this.message;
+
+    this.messagesInOrden.push(this.messageJSON);
+
+    this.messages = [];
+
+    for(let i = this.messagesInOrden.length; i > 0; i--){
+      this.messages.push(this.messagesInOrden[i - 1]);
+    }
+
+    this.contador++;
+    this.message = "";
+    this.messageJSON = {};
   }
 
 }
